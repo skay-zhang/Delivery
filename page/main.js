d8e89535980c79318ee8b2b240aaf1456c424b0a
Skay <skai-zhang@hotmail.com>
2022-07-30 20:56:33 +0800

    fix fonts bug

--- page/main.js
+++ page/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import router from './plugins/router.js'
+import '../public/fonts/ubuntu.css'
 import './style.css'
 
 createApp(App).use(router).mount('#app')
