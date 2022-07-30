d604e01e5a5def338477db24b9e54becd51e597a
Skay <skai-zhang@hotmail.com>
2022-07-30 23:16:41 +0800

    add mime util and image

---
+++ basic/plugins/mime.js
@@ -0,0 +1,28 @@
+let mime = {
+    apk: ["apk"],
+    assembly: ["class"],
+    audio: ["mp3", "au", "aif", "aifc", "wav"],
+    css: ["css"],
+    database: ["db"],
+    document: ["txt"],
+    excel: ["xls", "xla", "xlsx"],
+    exe: ["exe"],
+    html: ["html", "htm", "stm"],
+    ico: ["ico"],
+    image: ["bmp", "jpe", "jpeg", "jpg", "svg", "png", ""],
+    ipa: ["ipa"],
+    iso: ["iso"],
+    java: ["java"],
+    javascript: ["js"],
+    json: ["json"],
+    markdown: ["md"],
+    pdf: ["pdf"],
+    ppt: ["ppt", "pot", "pps"],
+    python: ["py"],
+    shell: ["sh"],
+    url: ["url"],
+    word: ["doc", "docx", "dot"],
+    zip: ["rar", "7z", "zip", "gz"]
+};
+
+export default mime;
